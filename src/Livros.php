c2e4b71efddd30bbe0886932571bb21d7af088c8
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2023-09-22 11:58:05 -0300

    Update Livros.php

--- src/Livros.php
+++ src/Livros.php
@@ -4,29 +4,75 @@ class Livros{
     private string $autor;
     private int $paginas = 0;
 
-
-    public function setTitulo(string $titulo):void{
-        $this->titulo = $titulo;
-    }
-
-    public function setAutor(string $autor):void{
-        $this->autor = $autor;
-    }
-
-    public function setPaginas(int $paginas):void{
-        $this->paginas = $paginas;
-    }
-
-    public function getTitulo():string{
+    /**
+     * Get the value of titulo
+     *
+     * @return string
+     */
+    public function getTitulo(): string
+    {
         return $this->titulo;
     }
 
-    public function getAutor():string{
+    /**
+     * Set the value of titulo
+     *
+     * @param string $titulo
+     *
+     * @return self
+     */
+    public function setTitulo(string $titulo): self
+    {
+        $this->titulo = $titulo;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of autor
+     *
+     * @return string
+     */
+    public function getAutor(): string
+    {
         return $this->autor;
     }
 
-    public function getPaginas():int{
+    /**
+     * Set the value of autor
+     *
+     * @param string $autor
+     *
+     * @return self
+     */
+    public function setAutor(string $autor): self
+    {
+        $this->autor = $autor;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of paginas
+     *
+     * @return int
+     */
+    public function getPaginas(): int
+    {
         return $this->paginas;
     }
 
+    /**
+     * Set the value of paginas
+     *
+     * @param int $paginas
+     *
+     * @return self
+     */
+    public function setPaginas(int $paginas): self
+    {
+        $this->paginas = $paginas;
+
+        return $this;
+    }
 }
